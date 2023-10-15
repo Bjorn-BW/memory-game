ab7970bc3a8bae2623a9618167723116dc10312d
Björn Bergqvist Wingren <bjorn.b.wingren@hotmail.se>
2023-10-15 13:32:15 +0200

    Added correct logic for flipping and removing cards

--- src/MemoryGame/Components/GameBoard.js
+++ src/MemoryGame/Components/GameBoard.js
@@ -8,52 +8,55 @@ const GameBoard = (props) => {
     const [cards, setCards] = useState(shuffleCards(generateCards()));
     const [selectedCards, setSelectedCards] = useState([]);
  
+    function onCardClicked (card) {
+        if (selectedCards.length > 1) {
+            return;
+        }
+        flipSingleCard(card);
+        if (selectedCards.length === 1){
+            const isMatch = selectedCards[0].color === card.color;
+            updateScore(isMatch);
+            setTimeout(() => {
+                if(isMatch){
+                    removeMatchedCards([card.id, selectedCards[0].id]);
+                } else {
+                    flipUnMatchedCards([selectedCards[0].id]);
+                }
+            },2000);
+        }
+    };
 
-
-    const onCardClicked = (e, card) => {
+    function flipSingleCard(card) {
         const updatedCards = [...cards];
         const cardIndex = updatedCards.findIndex(c => c.id === card.id);
-        
         updatedCards[cardIndex] = {
             ...updatedCards[cardIndex],
             isFlipped: !updatedCards[cardIndex].isFlipped
         }
-
-        if (selectedCards.length === 1){
-            const isMatch = selectedCards[0].color === updatedCards[cardIndex].color;
-            updateScore(isMatch);
-            const firstChoiceIndex = updatedCards.findIndex(c => c.id === selectedCards[0].id);
-            setTimeout(() =>{flipOrRemoveCards(isMatch, firstChoiceIndex, cardIndex, updatedCards)}, 2000);
-            
-            
-            setSelectedCards([]);
-            
-        } else {
-            setSelectedCards([...selectedCards, updatedCards[cardIndex]]);
-        }
+        setSelectedCards([...selectedCards, updatedCards[cardIndex]]);
         setCards(updatedCards);
-    };
-    function flipOrRemoveCards(isMatch, firstChoiceIndex, selectedCardIndex, updatedCards) {
-        if (!isMatch) {
-            updatedCards[firstChoiceIndex] = {
-                ...updatedCards[firstChoiceIndex],
-                isFlipped: !updatedCards[firstChoiceIndex].isFlipped
-            }
-            updatedCards[selectedCardIndex] = {
-                ...updatedCards[selectedCardIndex],
-                isFlipped: !updatedCards[selectedCardIndex].isFlipped
-            }
-        } else {
-            updatedCards[firstChoiceIndex] = {
-                ...updatedCards[firstChoiceIndex],
-                matchFound: !updatedCards[firstChoiceIndex].matchFound
-            }
-            updatedCards[selectedCardIndex] = {
-                ...updatedCards[selectedCardIndex],
-                matchFound: !updatedCards[selectedCardIndex].matchFound
-            }
-        }
     }
+
+    function removeMatchedCards(cardsMarkedForRemoval){
+        const updatedCards = cards.filter((card) => !cardsMarkedForRemoval.includes(card.id));
+        setCards(updatedCards);
+        setSelectedCards([]);
+
+    }
+
+    function flipUnMatchedCards(cardsToFlip){
+        const updatedCards = [...cards];
+        cardsToFlip.forEach(card => {
+            const cardIndex = updatedCards.findIndex(c => c.id === card);
+            updatedCards[cardIndex] = {
+                ...updatedCards[cardIndex],
+                isFlipped: !updatedCards[cardIndex].isFlipped
+            }
+        });
+        setCards(updatedCards);
+        setSelectedCards([]);
+    }
+
     return (
     <ul className="card-list">
         {
